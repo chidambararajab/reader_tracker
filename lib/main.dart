2be330fa8fc247e89c354c56f1e5e2a53bf1513a
Chid <chidambararaja97@gmail.com>
2024-06-14 23:58:35 +0530

    Model and screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:reader_tracker/pages/favorites_screen.dart';
+import 'package:reader_tracker/pages/home_screen.dart';
+import 'package:reader_tracker/pages/saved_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,6 +33,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  final List<Widget> _screens = <Widget>[
+    const HomeScreen(),
+    const SavedScreen(),
+    const FavoritesScreen()
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,12 +47,9 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("A.Reader"),
       ),
-      body: const Column(
-        children: [Text("Hello")],
-      ),
+      body: _screens[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        onTap: (index) {},
+        currentIndex: _currentIndex,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
@@ -58,8 +65,13 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        selectedItemColor: Theme.of(context).colorScheme.onPrimary,
-        unselectedItemColor: Theme.of(context).colorScheme.onSecondary,
+        selectedItemColor: Theme.of(context).colorScheme.onTertiaryFixed,
+        unselectedItemColor: Theme.of(context).colorScheme.primary,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
     );
   }
