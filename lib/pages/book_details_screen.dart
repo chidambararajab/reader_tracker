38adfb54ab87dcea4ec8587e74ddb458b6710046
Chid <chidambararaja97@gmail.com>
2024-06-15 18:42:42 +0530

    Book Detail

--- lib/pages/book_details_screen.dart
+++ lib/pages/book_details_screen.dart
@@ -15,12 +15,60 @@ class _BookDetailsScreenState extends State<BookDetailsScreen> {
     final args =
         ModalRoute.of(context)?.settings.arguments as BookDetailsArguements;
     final Book book = args.itemBook;
+    final textTheme = Theme.of(context).textTheme;
 
     return Scaffold(
       appBar: AppBar(
         title: const Text("Book Details"),
       ),
-      body: Text(book.title),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            children: [
+              if (book.imageLinks.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Image.network(book.imageLinks['thumbnail']!),
+                ),
+              Column(
+                children: [
+                  Text(
+                    book.title,
+                    style: textTheme.headlineLarge,
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    "Author: ${book.authors.join(', ')}",
+                    style: textTheme.titleMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    "Published Date: ${book.publishedDate}",
+                    style: textTheme.labelLarge,
+                  ),
+                  Text(
+                    "Page Count: ${book.pageCount}",
+                    style: textTheme.labelLarge,
+                  ),
+                  Text(
+                    "Language: ${book.language}",
+                    style: textTheme.labelLarge,
+                  ),
+                ],
+              ),
+              if (book.description.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    book.description,
+                    style: textTheme.bodyLarge,
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
